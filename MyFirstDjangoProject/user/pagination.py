e989e355bd38ec1f1c4adf1f7f2520fdc312e215
Zul <zulfa.rahma@indocyber.id>
2023-10-20 16:49:02 +0700

    update page size

--- MyFirstDjangoProject/user/pagination.py
+++ MyFirstDjangoProject/user/pagination.py
@@ -3,6 +3,6 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from rest_framework.pagination import LimitOffsetPagination 
 
 class StandardResultsPagination(PageNumberPagination):
-    page_size = 1
+    page_size = 10
     page_size_query_param = 'page_size'
     max_page_size = 1000
