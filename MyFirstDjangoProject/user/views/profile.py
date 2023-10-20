a38545b106fa0d13348d2be51eb3da46264ca7d7
Zul <zulfa.rahma@indocyber.id>
2023-10-20 16:47:59 +0700

    override default ModelViewSet functions

--- MyFirstDjangoProject/user/views/profile.py
+++ MyFirstDjangoProject/user/views/profile.py
@@ -1,3 +1,4 @@
+import json
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.decorators import action
@@ -11,26 +12,114 @@ class ProfileViewSet(viewsets.ModelViewSet):
     serializer_class = ProfileSerializer
     pagination_class = StandardResultsPagination
 
-    @action(detail=False)
-    def recent_profiles(self, request):
-        recent_profiles = Profile.objects.all().order_by("-account")
+    def retrieve(self, request, pk=None):
+        profile = Profile.objects.filter(pk=pk).first()
+        if not profile:
+            return Response({
+                "message"   : "Profile not found.",
+                "status"    : 0,
+                "data"      : None
+            })
+        
+        serializer = self.get_serializer(profile)
+        return Response({
+            "message"   : "Profile",
+            "status"    : 1,
+            "data"      : serializer.data
+        })
+    
+    def create(self, request):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({
+            "message"   : "New profile created.",
+            "status"    : 1,
+            "data"      : serializer.data
+        })
 
+        return Response({
+            "message"   : "New profile NOT created.",
+            "status"    : 0,
+            "data"      : serializer.errors
+        })
+    
+
+    def update(self, request, pk):
+        profile = Profile.objects.filter(pk=pk).first()
+
+        if not profile:
+            return Response({
+                "message"   : "Profile not found.",
+                "status"    : 0,
+                "data"      : None
+            })
+
+
+        serializer = self.get_serializer(profile, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response({
+            "message"   : "Profile updated.",
+            "status"    : 1,
+            "data"      : serializer.data
+            })
+        return Response({
+            "message"   : "Profile NOT updated.",
+            "status"    : 0,
+            "data"      : serializer.errors
+        })
+    
+
+
+    def destroy(self, request, pk):
+        profile = Profile.objects.filter(pk=pk).first()
+
+        if not profile:
+            return Response({
+                "message"   : "Profile not found.",
+                "status"    : 0,
+                "data"      : None
+            })
+        
+        profile.delete()
+
+        return Response({
+            "message"   : "Profile deleted.",
+            "status"    : 1,
+            "data"      : None
+            })
+                
+    
+    @action(detail=False, methods=["get"])         
+    def all(self, request):
+    
+        recent_profiles = Profile.objects.all().order_by("-account")
         page = self.paginate_queryset(recent_profiles)   
+    
         if page is not None:
-            serializer = self.get_serializer(page, many=True)
+            serializer = self.get_serializer(page, many=True)    
             return self.get_paginated_response(serializer.data)
         
         serializer = self.get_serializer(recent_profiles, many=True)
         return Response(serializer.data)
     
+    
+    
+
     @action(detail=False)
     def test_registered_phone_number(self, request):
+
         phone_number = request.GET.get('phone_number')
-        isRegistered = Profile.objects.filter(phone_number=phone_number).exists()
-        if isRegistered:
+        is_registered = Profile.objects.filter(phone_number=phone_number).exists()
+
+        if is_registered:
             return Response({
                 "message": "The phone number is registered under a profile"
             })
+
         return Response({
             "message": "The phone number is not registered under any profile"
         })
+    
