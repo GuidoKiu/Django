591edaa63b11e4ff8d0a23d1fa84ea189d741d12
Zul <zulfa.rahma@indocyber.id>
2023-10-20 22:46:14 +0700

    update profile_serializer

--- MyFirstDjangoProject/user/serializer/profile_serializer.py
+++ MyFirstDjangoProject/user/serializer/profile_serializer.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
 from user.models.profile import Profile
 from django.core import validators
 from django.core.exceptions import ValidationError
@@ -7,10 +8,6 @@ from django.utils import timezone
 def validate_date_of_birth(value):
     if value >= timezone.now().date():
         raise ValidationError('Date of birth must not be in the future.')
-
-def validate_unique_phone_numbers(value):
-    if Profile.objects.filter(phone_number=value).exists():
-        raise ValidationError("Phone number already registered under a profile.")
 
 class ProfileSerializer(serializers.ModelSerializer):
     first_name = serializers.CharField(
@@ -37,7 +34,10 @@ class ProfileSerializer(serializers.ModelSerializer):
         required=False,
         max_length=20,
         validators=[
-            validate_unique_phone_numbers,
+            # validate_unique_phone_numbers,
+            UniqueValidator(
+                queryset=Profile.objects.all(),
+                message="Phone number already registered under another profile."),
             validators.RegexValidator(
                 regex='^\d+$',
                 message='Phone number must contain only digits.',
