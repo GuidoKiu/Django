76702f50e657cb3bb8b5a71c098b748deda3bea5
Zul <zulfa.rahma@indocyber.id>
2023-10-20 13:04:03 +0700

    update model (Profile)

--- MyFirstDjangoProject/user/models/profile.py
+++ MyFirstDjangoProject/user/models/profile.py
@@ -5,14 +5,15 @@ from .account import Account
 
 class Profile(models.Model):
     first_name = models.CharField(max_length=50, null=False)
-    last_name = models.CharField(max_length=50)
-    phone_number = models.CharField(max_length=20, unique=True)
-    date_of_birth = models.DateField()
+    last_name = models.CharField(max_length=50, null=True, blank=True)
+    phone_number = models.CharField(max_length=20, unique=True, null=True, blank=True)
+    date_of_birth = models.DateField(null=True, blank=True)
     account = models.OneToOneField(
         Account,
         on_delete=models.CASCADE,
         blank=False
     )
+
     def __str__(self):
         if not self.last_name:
             return self.first_name
