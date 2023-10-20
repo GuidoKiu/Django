3e929022993c526cb6d6bc087a5884081744f2c3
Zul <zulfa.rahma@indocyber.id>
2023-10-20 09:23:16 +0700

    update model Profile

--- MyFirstDjangoProject/user/models/profile.py
+++ MyFirstDjangoProject/user/models/profile.py
@@ -4,7 +4,7 @@ from .account import Account
 
 
 class Profile(models.Model):
-    first_name = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=50, null=False)
     last_name = models.CharField(max_length=50)
     phone_number = models.CharField(max_length=20, unique=True)
     date_of_birth = models.DateField()
@@ -14,4 +14,7 @@ class Profile(models.Model):
         blank=False
     )
     def __str__(self):
+        if not self.last_name:
+            return self.first_name
         return "{} {}".format(self.first_name, self.last_name)
+        
